ac1fd8a627ac28d44d73bf4fc618cfd0bdb1fd11
Atharv-1610 <atharv.aras@bounteous.com>
2025-03-20 20:02:40 +0530

    Push files

---
+++ src/main/java/com/example/AccessFile/AccessFileApplication.java
@@ -0,0 +1,13 @@
+package com.example.AccessFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessFileApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/AccessFile/TwoSum.java
@@ -0,0 +1,5 @@
+package com.example.AccessFile;
+
+public class TwoSum {
+
+}
